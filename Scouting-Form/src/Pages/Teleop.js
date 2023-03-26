2705440c5c33f2d388175ca103a5e48d2f389298
Sumedh Karajagi <122140290+sumedh1k@users.noreply.github.com>
2023-03-25 22:13:06 -0700

    Add checkbox buttons for intake location
    Co-Authored-By: Fumi Okutsu <78628210+Easternspork@users.noreply.github.com>

--- Scouting-Form/src/Pages/Teleop.js
+++ Scouting-Form/src/Pages/Teleop.js
@@ -4,7 +4,7 @@ import ScoreButton from "../Forms/ScoreButton.js"
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
-function Teleop({getScore, incrementScore, completedForms, setCompletedForms}) {
+function Teleop({matchInfo, setMatchInfo, getScore, incrementScore, completedForms, setCompletedForms}) {
     const navigate = useNavigate()
 
 
@@ -16,6 +16,14 @@ function Teleop({getScore, incrementScore, completedForms, setCompletedForms}) {
             navigate('/autonomous')
         }
     })
+
+    function handleChange(e) {
+        setMatchInfo({
+            ...matchInfo,
+            [e.target.name]: e.target.value
+        })
+        console.log(matchInfo)
+    }
     
     function handleSubmit(event) {
         event.preventDefault()
@@ -33,6 +41,16 @@ function Teleop({getScore, incrementScore, completedForms, setCompletedForms}) {
                 <ScoreButton scoreType="Mid Teleop" getScore={getScore} incrementScore={incrementScore}></ScoreButton>
                 <ScoreButton scoreType="Low Teleop" getScore={getScore} incrementScore={incrementScore} isAuto={false}></ScoreButton>        
             </div>
+
+            <p>Where does the robot intake game pieces?</p>
+            <div onChange={handleChange} className="radioInput">
+                <input type="checkbox" value="floor" name="intakeLocation" /> floor
+                <input type="checkbox" value="singleSubstation" name="intakeLocation" /> single substation
+                <input type="checkbox" value="doubleSubstationD" name="intakeLocation" /> double substation: drop
+                <input type="checkbox" value="doubleSubstationS" name="intakeLocation" /> double substation: shelf
+                <input type="checkbox" value="none" name="intakeLocation" /> no intake
+            </div>
+            
             <ChargeStation scoreType="Charge Station Endgame" getScore={getScore} incrementScore={incrementScore}></ChargeStation>
             <MobilityButton scoreType="Teleop Mobility" getScore={getScore} incrementScore={incrementScore}></MobilityButton>
             <button type='submit' onClick={handleSubmit}>Match Info</button>
