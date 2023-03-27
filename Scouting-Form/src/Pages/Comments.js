99255790ac409480422ea844bfbe189fe9a91a1c
MBritto <109992316+mb-coding@users.noreply.github.com>
2023-03-27 12:43:42 -0600

    Rearrange comments and Modify driver comments question
    changed driver comments question to text response and moved it from teleop to comments

--- Scouting-Form/src/Pages/Comments.js
+++ Scouting-Form/src/Pages/Comments.js
@@ -27,7 +27,7 @@ function Comments({scouterComments, setScouterComments, completedForms, setCompl
     function handleSubmit(event) {
         event.preventDefault()
 
-        if (scouterComments.redFlags == '' || scouterComments.teleopComments == '' || scouterComments.uniqueComments == '' || scouterComments.autonomousComments == '' || scouterComments.additionalComments == '') {
+        if (scouterComments.driverComments == '' || scouterComments.redFlags == '' || scouterComments.teleopComments == '' || scouterComments.uniqueComments == '' || scouterComments.autonomousComments == '' || scouterComments.additionalComments == '') {
             setError('Enter valid fields')
         } else {
             setError('')
@@ -42,15 +42,16 @@ function Comments({scouterComments, setScouterComments, completedForms, setCompl
     return (
         <div id='scouterComments'>
             <h1 id='title'>Comments and Criticism:</h1>
+            
             <input
-                name = "teleopComments"
-                placeholder="Additional Teleop Comments:"
+                name = "driverComments"
+                placeholder="How good was their driver?"
                 onChange={handleChange}
                 type='text'
-                value={scouterComments.teleopComments}
+                value={scouterComments.driverComments}
                 className='inputField'
             ></input>
-            
+
             <input
                 name = "autonomousComments"
                 placeholder="Additional Autonomous Comments:"
@@ -61,12 +62,12 @@ function Comments({scouterComments, setScouterComments, completedForms, setCompl
             ></input>
 
             <input
-                name = "redFlags"
-                placeholder="Are there any red flags with this robot?"
-                type='text'
-                className='inputField'
+                name = "teleopComments"
+                placeholder="Additional Teleop Comments:"
                 onChange={handleChange}
-                value={scouterComments.redFlags}
+                type='text'
+                value={scouterComments.teleopComments}
+                className='inputField'
             ></input>
 
              <input
@@ -76,6 +77,15 @@ function Comments({scouterComments, setScouterComments, completedForms, setCompl
                 onChange={handleChange}
                 className='inputField'
                 value={scouterComments.uniqueComments}
+            ></input>
+
+            <input
+                name = "redFlags"
+                placeholder="Are there any red flags with this robot?"
+                type='text'
+                className='inputField'
+                onChange={handleChange}
+                value={scouterComments.redFlags}
             ></input>
 
             <input
