fd45b88a9cbece1267949552e588ef28fcf0187d
easternspork <lazoorcraft@gmail.com>
2023-04-08 00:14:31 -0700

    Fix missing credentials.json error in netlify

--- Backend/googleapi_credentials.sh
+++ Backend/googleapi_credentials.sh
@@ -2,7 +2,7 @@
 
 if [[ -f credentials.json ]] ; then rm credentials.json ; fi
 
-echo "{" >> auth_config.json
+echo "{" >> credentials.json
 echo "  \"type\": \"service_account\"," >> credentials.json
 echo "  \"project_id\": \"scoutingapp2023\"," >> credentials.json
 echo "  \"private_key_id\": \"${GAPI_PRIVATEKEYID}\"," >> credentials.json
@@ -13,4 +13,4 @@ echo "  \"auth_uri\": \"https://accounts.google.com/o/oauth2/auth\"," >> credent
 echo "  \"token_uri\": \"https://oauth2.googleapis.com/token\"," >> credentials.json
 echo "  \"auth_provider_x509_cert_url\": \"https://www.googleapis.com/oauth2/v1/certs\"," >> credentials.json
 echo "  \"client_x509_cert_url\": \"https://www.googleapis.com/robot/v1/metadata/x509/scoutingapp2023%40scoutingapp2023.iam.gserviceaccount.com\"" >> credentials.json
-echo "}" >> auth_config.json
+echo "}" >> credentials.json
--- Backend/src/api.js
+++ Backend/src/api.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const serverless = require('serverless-http')
 
-const googleSheets = require('./googleSheets')
+const googleSheets = require('../googleSheets')
 
 const app = express()
 const router = express.Router()
