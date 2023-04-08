db42c0701d477ab4dd8d4c76176b3fcb6c526321
easternspork <lazoorcraft@gmail.com>
2023-04-08 00:30:18 -0700

    update google credentials script

--- Backend/googleapi_credentials.sh
+++ Backend/googleapi_credentials.sh
@@ -8,7 +8,7 @@ echo "  \"project_id\": \"scoutingapp2023\"," >> credentials.json
 echo "  \"private_key_id\": \"${GAPI_PRIVATEKEYID}\"," >> credentials.json
 echo "  \"private_key\": \"${GAPI_PRIVATEKEY}\"," >> credentials.json
 echo "  \"client_email\": \"scoutingapp2023@scoutingapp2023.iam.gserviceaccount.com\"," >> credentials.json
-echo "  \"client_id\": \"111133545601341528826\"," >> credentials.json
+echo "  \"client_id\": \"103254138460037072034\"," >> credentials.json
 echo "  \"auth_uri\": \"https://accounts.google.com/o/oauth2/auth\"," >> credentials.json
 echo "  \"token_uri\": \"https://oauth2.googleapis.com/token\"," >> credentials.json
 echo "  \"auth_provider_x509_cert_url\": \"https://www.googleapis.com/oauth2/v1/certs\"," >> credentials.json
